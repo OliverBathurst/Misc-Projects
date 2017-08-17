6316853d61c95912350b565e9b78eb923e5f5c2c
OliverMBathurst <oliverbathurst12345@gmail.com>
2017-08-17 02:41:41 +0100

    Update The Collatz Conjecture.c

--- The Collatz Conjecture/The Collatz Conjecture.c
+++ The Collatz Conjecture/The Collatz Conjecture.c
@@ -1,21 +1,20 @@
-// The Collatz Conjecture.cpp : Defines the entry point for the console application.
+// The Collatz Conjecture.c : Defines the entry point for the console application.
 //
 #include <stdio.h>
 #include <conio.h>
 
-void compute(int n);
-int number = 0, steps = 1; //declare two vars, the number being used and no. of steps
+int compute(int n);
+int number = 0, steps = 0; //declare two vars, the number being used and no. of steps
 
 void main(){
 	printf("Enter your number: ");
 	while(number < 1){ //loop while the inputted is above 1
 		scanf_s("%d", &number); //scan it
 	}
-	compute(number); //compute
-	printf("TOTAL NUMBER OF STEPS: %d", steps); //display
+	printf("TOTAL NUMBER OF STEPS: %d", compute(number)); //display
 	getch(); //pause
 }
-void compute(int n){
+int compute(int n){
 	if(n!=1){ //if it hasn't finished
 		if(n % 2 == 0){ //if even
 			n = n / 2;	//divide number by two	
@@ -25,4 +24,5 @@ void compute(int n){
 		printf("Number: %d, Step count: %d\n", n, steps++); //update the user, increment steps
 		compute(n); //recur
 	}//if n==1, function finishes and control goes back to main
+	return steps;
 }
